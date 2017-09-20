8ce76ea1defd8d74176d1c90b0cdff7b26609baa
sajikr1 <sajikr1@yahoo.com>
2017-09-19 23:17:00 -0700

    data input for 1 layout completed

--- src/application/TwoPurchase_childController.java
+++ src/application/TwoPurchase_childController.java
@@ -28,28 +28,63 @@ import javafx.scene.control.cell.TextFieldTreeTableCell;
 public class TwoPurchase_childController implements Initializable {
 
 	@FXML TableView<Table> tableID;
-	
 	@FXML TableColumn<Table,Integer > Sno_Id;//iID;	
-	@FXML 
-	//	TableColumn<Table,Integer > iName;
-	 TableColumn<Table,String> iName;
-
-	//@FXML TableColumn<Table,Integer > iDate;
+	@FXML TableColumn<Table,String> iTypeNote;
+	@FXML TableColumn<Table,String> iCreditDebitNo;
+	@FXML TableColumn<Table,String> iCreditDebitDate;
+	@FXML TableColumn<Table,String> iOriginalInvoice_No;
+	@FXML TableColumn<Table,String> iOriginalInvoice_Date;
+	@FXML TableColumn<Table,String> iOriginalInvoice_HSN;
+	@FXML TableColumn<Table,String> iOriginalInvoice_Goods;
+	@FXML TableColumn<Table,String> iDifferential_Value;
+	@FXML TableColumn<Table,String> iIgst_Rate;
+	@FXML TableColumn<Table,String> iIgst_Amount;
+	@FXML TableColumn<Table,String> iCgst_Rate;
+	@FXML TableColumn<Table,String> iCgst_Amount;
+	@FXML TableColumn<Table,String> iSgst_Rate;
+	@FXML TableColumn<Table,String> iSgst_Amount;
+	@FXML TableColumn<Table,String> iReverse;
 	@FXML TableColumn<Table,String > iGSTIN_UIN_Id;
 	
-	//@FXML TableColumn<Table,Integer > iPrice;
+	public TextField gstinuinInput;
+	public TextField TypeNoteInput;
+	public TextField CreditDebitNoInput;
+	public TextField CreditDebitDateInput;
+	public TextField OriginalInvoice_NoInput;
+	public TextField OriginalInvoice_DateInput;
+	public TextField OriginalInvoice_HSNInput;
+	public TextField OriginalInvoice_GoodsInput;
+	public TextField Differential_ValueInput;
+	public TextField IGST_RateInput;
+	public TextField IGST_AmountInput;
+	public TextField CGST_RateInput;
+	public TextField CGST_AmountInput;
+	public TextField SGST_RateInput;
+	public TextField SGST_AmountInput;
+	public TextField ReverseInput;
 	
-	public TextField nameInput;
-	public TextField gstinuinInput; 
-	
-	@FXML
-	Button Submit;
+	@FXML Button Submit;
 	
 	private int iNumber = 1;
 	public void onAddItem(ActionEvent event ){
-	Table entry  = new Table(iNumber,gstinuinInput.getText(), nameInput.getText());
+	Table entry  = new Table(iNumber,gstinuinInput.getText(), 
+			TypeNoteInput.getText(),
+			CreditDebitNoInput.getText(),
+			CreditDebitDateInput.getText(),
+			OriginalInvoice_NoInput.getText(),
+			OriginalInvoice_DateInput.getText(),
+			OriginalInvoice_HSNInput.getText(),
+			OriginalInvoice_GoodsInput.getText(),
+			Differential_ValueInput.getText(),
+			IGST_RateInput.getText(),
+			IGST_AmountInput.getText(),
+			CGST_RateInput.getText(),
+			CGST_AmountInput.getText(),
+			SGST_RateInput.getText(),
+			SGST_AmountInput.getText(),
+			ReverseInput.getText());//
+
 	iNumber++;
-	
 	data.add(entry);
 	clearForm();
 	}
@@ -57,13 +92,10 @@ public class TwoPurchase_childController implements Initializable {
 	private void clearForm() {
 		// TODO Auto-generated method stub
 		//nameInput.clear();
-		System.out.println("clearForm called bbbbbbbbb : "+ nameInput.getText());
 	}
-
 	
-	
-	final ObservableList<Table> data = FXCollections.observableArrayList(
-			new Table(iNumber ++, "rGstinuin","Name_1"));//, "09/01/2017")
+	final ObservableList<Table> data = FXCollections.observableArrayList();
+			//new Table(iNumber ++, "rGstinuin","Name_1"));//, "09/01/2017")
 			
 	
 	@Override
@@ -72,21 +104,27 @@ public class TwoPurchase_childController implements Initializable {
 		
 		//iID.setCellValueFactory(new PropertyValueFactory<Table,Integer>("rID"));
 		Sno_Id.setCellValueFactory(new PropertyValueFactory<Table,Integer>("rID"));
-		
-		//iID.se(iID);
-		//iName.setCellValueFactory(new PropertyValueFactory<Table,Integer>("rName"));
 		iGSTIN_UIN_Id.setCellValueFactory(new PropertyValueFactory<Table,String>("rGstinuin"));
-		
-		iName.setCellValueFactory(new PropertyValueFactory<Table,String>("rName"));
+		iTypeNote.setCellValueFactory(new PropertyValueFactory<Table,String>("rTypenote"));
+		iCreditDebitNo.setCellValueFactory(new PropertyValueFactory<Table,String>("rCreditdebitno"));
+		iCreditDebitDate.setCellValueFactory(new PropertyValueFactory<Table,String>("rCreditdebitdate"));
+		iOriginalInvoice_No.setCellValueFactory(new PropertyValueFactory<Table,String>("rOriginalinvoiceno"));
+		iOriginalInvoice_Date.setCellValueFactory(new PropertyValueFactory<Table,String>("rOriginalinvoicedate"));
+		iOriginalInvoice_HSN.setCellValueFactory(new PropertyValueFactory<Table,String>("rOriginalinvoicehsn"));
+		iOriginalInvoice_Goods.setCellValueFactory(new PropertyValueFactory<Table,String>("rOriginalinvoicegoods"));
+		iDifferential_Value.setCellValueFactory(new PropertyValueFactory<Table,String>("rDifferentialvalue"));
+		iIgst_Rate.setCellValueFactory(new PropertyValueFactory<Table,String>("rIgstrate"));
+		iIgst_Amount.setCellValueFactory(new PropertyValueFactory<Table,String>("rIgstamount"));
+		iCgst_Rate.setCellValueFactory(new PropertyValueFactory<Table,String>("rCgstrate"));
+		iCgst_Amount.setCellValueFactory(new PropertyValueFactory<Table,String>("rCgstamount"));
+		iSgst_Rate.setCellValueFactory(new PropertyValueFactory<Table,String>("rSgstrate"));
+		iSgst_Amount.setCellValueFactory(new PropertyValueFactory<Table,String>("rSgstamount"));
+		iReverse.setCellValueFactory(new PropertyValueFactory<Table,String>("rReverse"));
 		
 		tableID.setItems(data);
 		tableID.setEditable(true);
 		
 		iGSTIN_UIN_Id.setCellFactory(TextFieldTableCell.forTableColumn());
-		
-		iName.setCellFactory(TextFieldTableCell.forTableColumn());
-		
-		//iID.setCellFactory(TextFieldTableCell.forTableColumn());
 		System.out.println("initialize called zzzzzzzzzzz");
 		    
 	}
@@ -94,7 +132,8 @@ public class TwoPurchase_childController implements Initializable {
 	public void changeFirstNameCellEvent(CellEditEvent editCell)
 	{
 		Table  personSelected = tableID.getSelectionModel().getSelectedItem(); 
-		personSelected.setRName(editCell.getNewValue().toString());
+	//	personSelected.setRName(editCell.getNewValue().toString());
+		personSelected.setRGstinuin(editCell.getNewValue().toString());
 				
 	}
 	/*public void changeIDCellEvent(CellEditEvent editCell)
--- src/model/Table.java
+++ src/model/Table.java
@@ -14,16 +14,58 @@ public class Table
 {
 
 	private final SimpleIntegerProperty rID;
-	private final SimpleStringProperty rName;
-/*	private final SimpleStringProperty rDate;
-	private final SimpleIntegerProperty rPrice;
-*/	
-	public Table(int rID, String rName){//, String rDate, Integer rPrice){
+	private final SimpleStringProperty rGstinuin;
+	private final SimpleStringProperty rTypenote;
+	private final SimpleStringProperty rCreditdebitno;
+	private final SimpleStringProperty rCreditdebitdate;
+	private final SimpleStringProperty rOriginalinvoiceno;
+	private final SimpleStringProperty rOriginalinvoicedate;
+	private final SimpleStringProperty rOriginalinvoicehsn;
+	private final SimpleStringProperty rOriginalinvoicegoods;
+	private final SimpleStringProperty rDifferentialvalue;
+	private final SimpleStringProperty rIgstrate;
+	private final SimpleStringProperty rIgstamount;
+	private final SimpleStringProperty rCgstrate;
+	private final SimpleStringProperty rCgstamount;
+	private final SimpleStringProperty rSgstrate;
+	private final SimpleStringProperty rSgstamount;
+	private final SimpleStringProperty rReverse;
+	
+	public Table(int rID, String rGstinuin,/*String rName,*/
+		String TypeNoteInput,
+		String CreditDebitNoInput,
+		String CreditDebitDateInput,
+		String OriginalInvoice_NoInput,
+		String OriginalInvoice_DateInput,
+		String OriginalInvoice_HSNInput,
+		String OriginalInvoice_GoodsInput,
+		String Differential_ValueInput,
+		String IGST_RateInput,
+		String IGST_AmountInput,
+		String CGST_RateInput,
+		String CGST_AmountInput,
+		String SGST_RateInput,
+		String SGST_AmountInput,
+		String ReverseInput
+		){
 		this.rID = new SimpleIntegerProperty(rID);
-		this.rName = new SimpleStringProperty(rName);
-		/*this.rDate= new SimpleStringProperty(rDate);
-		this.rPrice = new SimpleIntegerProperty(rPrice);
-		*/
+		this.rGstinuin = new SimpleStringProperty(rGstinuin);
+		this.rTypenote=  new SimpleStringProperty(TypeNoteInput);
+		this.rCreditdebitno=  new SimpleStringProperty(CreditDebitNoInput);
+		this.rCreditdebitdate=  new SimpleStringProperty(CreditDebitDateInput);
+		this.rOriginalinvoiceno=  new SimpleStringProperty(OriginalInvoice_NoInput);
+		this.rOriginalinvoicedate=  new SimpleStringProperty(OriginalInvoice_DateInput);
+		this.rOriginalinvoicehsn=  new SimpleStringProperty(OriginalInvoice_HSNInput);
+		this.rOriginalinvoicegoods=  new SimpleStringProperty(OriginalInvoice_GoodsInput);
+		this.rDifferentialvalue=  new SimpleStringProperty(Differential_ValueInput);
+		this.rIgstrate=  new SimpleStringProperty(IGST_RateInput);
+		this.rIgstamount=  new SimpleStringProperty(IGST_AmountInput);
+		this.rCgstrate=  new SimpleStringProperty(CGST_RateInput);
+		this.rCgstamount=  new SimpleStringProperty(CGST_AmountInput);
+		this.rSgstrate=  new SimpleStringProperty(SGST_RateInput);
+		this.rSgstamount=  new SimpleStringProperty(SGST_AmountInput);
+		this.rReverse=  new SimpleStringProperty(ReverseInput);
+		
 	}
 	
 	public Integer getRID(){
@@ -34,11 +76,125 @@ public class Table
 		rID.set(val);		
 	}
 	
-	public String getRName(){
-		return rName.get();
+	public String getRGstinuin(){
+		return rGstinuin.get();
 	}
-	public void setRName(String val)
+	public void setRGstinuin(String val)
 	{
-		rName.set(val);		
+		rGstinuin.set(val);		
 	}
+	
+	public String getRTypenote(){
+		return rTypenote.get();
+	}
+	public void setRTypenote(String val)
+	{
+		rTypenote.set(val);		
+	}
+	
+	public String getRCreditdebitno(){
+		return rCreditdebitno.get();
+	}
+	public void setRCreditdebitno(String val)
+	{
+		rCreditdebitno.set(val);		
+	}
+	
+	public String getRCreditdebitdate(){
+		return rCreditdebitdate.get();
+	}
+	public void setRCreditdebitdate(String val)
+	{
+		rCreditdebitdate.set(val);		
+	}
+	
+	public String getROriginalinvoiceno(){
+		return rOriginalinvoiceno.get();
+	}
+	public void setROriginalinvoiceno(String val)
+	{
+		rOriginalinvoiceno.set(val);		
+	}
+	
+	public String getROriginalinvoicedate(){
+		return rOriginalinvoicedate.get();
+	}
+	public void setROriginalinvoicedate(String val)
+	{
+		rOriginalinvoicedate.set(val);		
+	}
+	
+	public String getROriginalinvoicehsn(){
+		return rOriginalinvoicehsn.get();
+	}
+	public void setROriginalinvoicehsn(String val)
+	{
+		rOriginalinvoicehsn.set(val);		
+	}
+	
+	public String getROriginalinvoicegoods(){
+		return rOriginalinvoicegoods.get();
+	}
+	public void setROriginalinvoicegoods(String val)
+	{
+		rOriginalinvoicegoods.set(val);		
+	}
+	
+	public String getRDifferentialvalue(){
+		return rDifferentialvalue.get();
+	}
+	public void setRDifferentialvalue(String val)
+	{
+		rDifferentialvalue.set(val);		
+	}
+	public String getRIgstrate(){
+		return rIgstrate.get();
+	}
+	public void setRIgstrate(String val)
+	{
+		rIgstrate.set(val);		
+	}
+	public String getRIgstamount(){
+		return rIgstamount.get();
+	}
+	public void setRIgstamount(String val)
+	{
+		rIgstamount.set(val);		
+	}
+	public String getRCgstrate(){
+		return rCgstrate.get();
+	}
+	public void setRCgstrate(String val)
+	{
+		rCgstrate.set(val);		
+	}
+	public String getRCgstamount(){
+		return rCgstamount.get();
+	}
+	public void setRCgstamount(String val)
+	{
+		rCgstamount.set(val);		
+	}
+	public String getRSgstrate(){
+		return rSgstrate.get();
+	}
+	public void setRSgstrate(String val)
+	{
+		rSgstrate.set(val);		
+	}
+	public String getRSgstamount(){
+		return rSgstamount.get();
+	}
+	public void setRSgstamount(String val)
+	{
+		rSgstamount.set(val);		
+	}
+	public String getRReverse(){
+		return rReverse.get();
+	}
+	public void setRReverse(String val)
+	{
+		rReverse.set(val);		
+	}
+
 }
